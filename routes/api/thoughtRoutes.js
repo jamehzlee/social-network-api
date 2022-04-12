36044908e633e922ef57cec838d95ca8650b1c3e
Jamehzz <probableeme@gmail.com>
2022-04-12 17:53:31 -0400

    created userRoutes and thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('thoughts/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
+module.exports = router;
