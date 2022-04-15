6be4f02d88741bacf0ae49f18bcab86eab0bd576
Jamehzz <probableeme@gmail.com>
2022-04-15 01:50:49 -0400

    removed redundant template literal

--- models/User.js
+++ models/User.js
@@ -17,13 +17,13 @@ const userSchema = new Schema(
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Thought',
+        ref: 'thought',
       },
     ],  
     friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'user',
       },
     ],
   },
@@ -38,7 +38,7 @@ const userSchema = new Schema(
 userSchema
   .virtual('friendCount')
   .get(function () {
-    return `${this.friends.length}`;
+    return this.friends.length;
   });
 
 const User = model('user', userSchema);
